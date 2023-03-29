dd34c6b9f0d516b1a2980d3b2fa4363bbf3772da
Dariia Lavinska <d.lavinska@gmail.com>
2023-03-29 12:07:04 +0300

    add 5hw for test

---
+++ 5hw/script/script.js
@@ -0,0 +1,9 @@
+const a = parseInt(prompt("Введіть перше число", " "));
+const b = parseInt(prompt("Введіть друге число", " "));
+
+const s = a + b;
+const v = a - b;
+const m = a * b;
+const d = a / b;
+
+alert(`${a} + ${b} = ${s}\n${a} - ${b} = ${v}\n${a} * ${b} = ${m}\n${a} / ${b} = ${d}`);
