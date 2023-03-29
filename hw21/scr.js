85b0169600dbefd9d59ace23abd9464270a5ae0a
Dariia Lavinska <d.lavinska@gmail.com>
2023-03-29 10:33:47 +0300

    addtest123

--- hw21/scr.js
+++
@@ -1,6 +0,0 @@
-const num = +prompt("Enter a five-digit number", " ");
-
-const arr = [..." " + num];
-const number = arr.join(" ");
-
-alert(number);
--- test2/script/st.js
+++
@@ -1,6 +0,0 @@
-let hour = prompt("Введіть кількість годин без хвилин, щоб дізнатись скільки в цій кількості секунд:");
-hour = Number(hour);
-
-const seconds = hour * 3600;
-
-alert(seconds);
