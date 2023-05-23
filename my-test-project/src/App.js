f20685bac933c1c5073941b0d205fb29ecd85453
Dariia Lavinska <d.lavinska@gmail.com>
2023-05-23 13:48:21 +0300

    add my files

--- my-test-project/src/App.js
+++ my-test-project/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import React from 'react';
+import Main from './components/Main'
 function App() {
   return (
     <div className="App">
-      
+      <Main />
     </div>
   );
 }
